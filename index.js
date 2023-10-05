f8bcfb9b2214eccc1c0445eb0e9e2b5f6250fd33
sammyifeanyi <145140412+sammyifeanyi@users.noreply.github.com>
2023-10-05 16:34:47 +0100

    First Javascript file on my github

---
+++ index.js
@@ -0,0 +1,99 @@
+// // String Function that changes capslock
+// let myname = prompt("what is your name?")
+// console.log(myname.toUpperCase());
+
+// let myname1 = prompt("what is your name?")
+// console.log(myname1.toLowerCase());
+
+// // A program that slice the strings and differentiate them
+// let fullname = prompt ("what is your fullname?");
+// let firstname;
+// let lastname;
+
+// // firstname = fullname.slice(0, 3);
+// // lastname = fullname.slice(4);
+
+// firstname = fullname.slice(0, fullname.indexOf(" "));
+// lastname = fullname.slice(fullname.indexOf(" ") + 1);
+
+// console.log(firstname);
+// console.log(lastname);
+
+// A program that takes a user's word in small letters
+// let b = prompt("Enter something");
+// if(b == lowercase){
+//     console.log("Good");
+// }
+// else {
+//     console.log("please put in only small letters");
+// }
+
+
+// A program that tells a user to make a sentence
+// let sentence = prompt("make a sentence");
+// let sentence1 = sentence.toUpperCase();
+// let sentence3 = sentence.charAt(0);
+// console.log(sentence1);
+
+
+// A program that 
+// let w1 = prompt("Enter a word");
+// w1 = w1.toLowerCase();
+
+// let w2 = prompt("Enter a word");
+// w2 = w2.toLowerCase();
+// console.log("small:", w2);
+// // w2 = w2[0].toUpperCase();
+// console.log(w2[0]);
+// let p1 = w2[0].toUpperCase();
+// w2[0]= p1
+// console.log(w2[0]);
+// w2[0] = w2[0].toUpperCase();
+// console.log(w2[0]);
+
+// let l3 = "SAM";
+// let p = l3.toLowerCase();
+// console.log(l3);
+// console.log(p);
+// l3 = p;
+// console.log(l3);
+// console.log(l3[0]);
+// console.log(l3[0].toUpperCase());
+// let k = l3[0].toUpperCase();
+// l3 = l3.replace(l3[0], k);
+// // console.log(l33);
+// console.log(l3);
+
+// l3 = l33;
+// console.log("l3", l3);
+
+
+// let o = w3[0].toUpperCase();
+// console.log(o);
+
+// console.log(w1 + " " + w2 + " " + w3);
+
+// let p = "samuel";
+// console.log(p);
+// let answer = p.replace("m", "l");
+// console.log(answer);
+
+
+// let m = "  samuel   ";
+// m = m.trim();
+// let n = " ifeanyi    ";
+// n = n.trim();
+// let mn = m + " " + n;
+// console.log(mn);
+
+// let m = "fantastic";
+// let m2 = m.slice(3, 5);
+// console.log(m2);
+
+// let fn = "samuel ifeanyi";
+// let fn1 = fn.split("", 3);
+// console.log("My name is", fn1[0])
+// console.log(fn1);
+
+
+
